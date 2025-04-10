f28a201edba7bc0436cc9b7d2dc235211e732e1b
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-04-10 00:12:30 +0000

    Added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   root "photos#index"
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
   devise_for :users
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
